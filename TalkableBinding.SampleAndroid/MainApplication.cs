55a0c3822e2600876bc0593b9983c71cf8dd5988
Dmitrii Khokhlov <dmitriik@ifit.com>
2021-12-07 06:27:45 -0600

    add android binding+sample

---
+++ TalkableBinding.SampleAndroid/MainApplication.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Talkable.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TalkableBinding.SampleAndroid
+{
+	[Application]
+	public class MainApplication : Application
+	{
+		protected MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
+
+		public override void OnCreate()
+		{
+			base.OnCreate();
+			Talkable.Initialize(this);
+		}
+	}
+}
